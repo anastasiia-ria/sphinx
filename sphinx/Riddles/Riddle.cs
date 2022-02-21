a753269b36d1e40a6293a5d8928762a2be2e183b
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-21 11:23:22 -0800

    add Riddle class to the project
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

---
+++ sphinx/Riddles/Riddle.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Sphinx.Riddles
+{
+  public class Riddle
+  {
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public Riddle(string question, string answer)
+    {
+      Question = question;
+      Answer = answer;
+    }
+    public bool Correct(string userAnswer)
+    {
+      return (userAnswer == Answer);
+    }
+  }
+}
